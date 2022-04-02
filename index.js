44b9cbe86570ff107d19997813006c0f491e9eec
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-04-02 21:14:54 +0530

    generated 21 squares

--- index.js
+++ index.js
@@ -13,17 +13,36 @@ let squareData = {
   margin: 5,
 };
 
-svg
-  .selectAll("rect")
-  .data([squareData])
-  .enter()
-  .append("rect")
-  .attr("height", (d) => d.height)
-  .attr("width", (d) => d.width)
-  .attr("x", (d) => d.x)
-  .attr("y", (d) => d.y)
-  .attr("fill", (d) => d.color)
-  .attr("margin", (d) => d.margin);
+let makeDigit = () => {
+  let digitData = [];
+  for (let i = 1; i <= 7; i++) {
+    for (let j = 1; j <= 3; j++) {
+      let currSquare = { ...squareData };
+      currSquare.x = j * (currSquare.width + currSquare.margin);
+      currSquare.y = i * (currSquare.width + currSquare.margin);
+
+      digitData.push(currSquare);
+    }
+  }
+  return digitData;
+};
+
+let render = () => {
+  let digitData = makeDigit();
+  svg
+    .selectAll("rect")
+    .data(digitData)
+    .enter()
+    .append("rect")
+    .attr("height", (d) => d.height)
+    .attr("width", (d) => d.width)
+    .attr("x", (d) => d.x)
+    .attr("y", (d) => d.y)
+    .attr("fill", (d) => d.color)
+    .attr("margin", (d) => d.margin);
+};
+
+render();
 
 // var cnt = 0;
 // setInterval(() => {
